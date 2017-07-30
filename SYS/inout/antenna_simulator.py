d2f83788f54e76c66e72d15a8348c8dcbd2962ad
detialiaj <detialiaj@gmail.com>
2017-07-31 00:33:55 +0200

    add_entry

--- SYS/inout/antenna_simulator.py
+++ SYS/inout/antenna_simulator.py
@@ -8,5 +8,18 @@ class EntryForm(ModelForm):
         fields = ['entry_type', 'rfid', 'structure']
 
 def add_entry(request):
-    message = 'its ok!'
-    return HttpResponse(message)
+    from inout.models import Entry, Structure
+    from main.models import RFIDtag
+    from datetime import datetime
+    #message = 'its ok!'
+
+    r=RFIDtag.objects.all() #should filter the exact tag by value and return it pk
+    s=Structure.objects.all() #should filter the exact Structure by code and return it pk
+    e = Entry(
+        entry_type='In',
+        rfid=r.get(pk='2'),
+        action_date=datetime.now(),
+        structure=s.get(pk=2)
+        )
+    e.save()
+    return HttpResponse(e)
